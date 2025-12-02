3f4bf945814ddefd660497457c4716271109e42f
Denscape <dnmrrdnd@gmail.com>
2025-12-03 02:08:40 +0800

    Add HTML and JavaScript files

---
+++ js_2/index.js
@@ -0,0 +1,8 @@
+let herName = "Alenere";
+let hisName = "David";
+let girl = "she";
+let drinks = "coffee";
+
+alert(herName + ", " + " the friendly neighbor, waved at " + hisName + " as " + girl + " walked by " 
+    + hisName + "'s house. " + hisName + " smiled back and invited " + herName + " in for a cup of " 
+    + drinks + ".");
---
+++ js_3/script.js
@@ -0,0 +1,7 @@
+let eng = 92;
+let math = 91;
+let science = 89;
+let filipino = 92;
+let ap = 85;
+
+document.getElementById("text").innerHTML = "your average is: " + (eng + math + science + filipino + ap) / 5;
